8d3227ce409feaf054d2b4fb00ac6f2598a81939
shrek206 <kevin.grignard.pro@gmail.com>
2026-03-25 15:39:53 +0100

    Creation Footer et Routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Acceuil } from './Features/acceuil/acceuil';
+import { Evenements } from './Features/evenements/evenements';
+import { Contact } from './Features/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'acceuil',
+        component: Acceuil
+    },
+
+    {
+        path: '',
+        redirectTo: "acceuil",
+        pathMatch: 'full'
+    },
+
+    {
+        path: 'evenements',
+        component: Evenements
+    },
+
+    {
+        path: 'contact',
+        component: Contact
+    }
+];
