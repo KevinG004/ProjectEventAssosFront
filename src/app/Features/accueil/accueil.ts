b084548b93836c0b2fed05cbd77b6038312cbb39
shrek206 <kevin.grignard.pro@gmail.com>
2026-03-26 10:13:31 +0100

    petit changement 4

--- src/app/Features/accueil/accueil.ts
+++ src/app/Features/accueil/accueil.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-acceuil',
+  selector: 'app-accueil',
   imports: [],
   templateUrl: './accueil.html',
   styleUrl: './accueil.css',
