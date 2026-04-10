1696c38c948472d905ff6e33895464df967c0c4f
Luca Giglio <Lucagiglio2507@gmail.com>
2026-04-10 14:41:09 +0200

    Hello Kev bon Chance ajout des modeles et finition de tout ce qui est rapport au user

---
+++ src/app/Shared/Models/user-model/user-model-request.ts
@@ -0,0 +1,23 @@
+export interface User {
+  userName: string;
+  birthDate: Date; 
+  gender: string; 
+}
+
+export interface UserFirstLogin {
+  userName: string;
+  password: string;
+  birthDate: Date; 
+  gender: string;
+  passwordChanged: boolean;
+}
+
+export interface UserLogin {
+  identifiant: string;
+  password: string;
+}
+
+export interface UserRegister {
+  email: string;
+  roleId: number;
+}
